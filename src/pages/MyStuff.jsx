897581b44a19e60ab03c8494e440f144532b1e3c
fdxruli <48109415+fdxruli@users.noreply.github.com>
2025-10-22 10:57:42 -0600

    Add files via upload

--- src/pages/MyStuff.jsx
+++ src/pages/MyStuff.jsx
@@ -149,7 +149,7 @@ const RewardsSection = ({ customerId }) => {
 
                 <p className={styles.progressText}>
                     {noLevelsConfigured
-                        ? "El sistema de recompensas se está preparando. ¡Vuelve pronto!"
+                        ? "El sistema de recompensas se está preparando. ¡Vuelve pronto! Pero, puedes compartir tu link e ir acumulando puntos"
                         : hasReachedMaxLevel
                             ? "¡Has alcanzado el nivel más alto! Eres un(a) crack. 😎"
                             : `Necesitas ${next_level.min_referrals - referral_count} referidos más para el siguiente nivel.`
