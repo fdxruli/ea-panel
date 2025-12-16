0c83f8b86ed322e7b645c5cb52d5485105925470
fdxruli <48109415+fdxruli@users.noreply.github.com>
2025-12-15 18:43:40 -0600

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' 
+import './index.css'
+import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>,
+)
