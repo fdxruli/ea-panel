77a65c21328a617d911cd5e1e627221beff4d63e
fdxruli <rulisebastian8009@gmail.com>
2026-09-10 18:36:52 -0500

    Fix duplicate types in baseline candidate

--- scratch/baseline_test/baseline_test_candidate.sql
+++ scratch/baseline_test/baseline_test_candidate.sql
@@ -299,27 +299,6 @@ CREATE TABLE public.settings (
 );
 
 
--- Tipos Enums base
-CREATE TYPE public.order_status AS ENUM (
-    'pendiente',
-    'confirmado',
-    'en_preparacion',
-    'listo_para_entregar',
-    'completado',
-    'cancelado'
-);
-
-CREATE TYPE public.discount_type AS ENUM (
-    'general',
-    'product',
-    'category'
-);
-
-CREATE TYPE public.admin_role AS ENUM (
-    'superadmin',
-    'admin',
-    'staff'
-);
 
 -- Tipo Compuesto base
 CREATE TYPE public.cart_item AS (
